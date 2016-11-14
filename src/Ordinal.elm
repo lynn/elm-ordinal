364a1f24d58bd591b218fcf4ca1206663d0c3c19
opsb <oliver@opsb.co.uk>
2016-11-14 22:38:07 +0100

    Upgrade to elm 0.18

--- src/Ordinal.elm
+++ src/Ordinal.elm
@@ -1,6 +1,8 @@
-module Ordinal exposing
-    ( ordinal, ordinalSuffix
-    )
+module Ordinal
+    exposing
+        ( ordinal
+        , ordinalSuffix
+        )
 
 {-| A library for converting integers (`23`, `0`, `-2`) to English ordinal
 strings (`"23rd"`, `"0th"`, `"-2nd"`).
@@ -13,6 +15,7 @@ strings (`"23rd"`, `"0th"`, `"-2nd"`).
 
 {- Ordinal -}
 
+
 {-| Get the English ordinal suffix (st/nd/rd/th) for a given integer.
 
     ordinalSuffix 42 == "nd"
@@ -22,16 +25,24 @@ strings (`"23rd"`, `"0th"`, `"-2nd"`).
 ordinalSuffix : Int -> String
 ordinalSuffix n =
     let
-        n' = abs n
+        n_ =
+            abs n
     in
-        if n' % 100 // 10 == 1 then
+        if n_ % 100 // 10 == 1 then
             "th"
         else
-            case n' % 10 of
-                1 -> "st"
-                2 -> "nd"
-                3 -> "rd"
-                _ -> "th"
+            case n_ % 10 of
+                1 ->
+                    "st"
+
+                2 ->
+                    "nd"
+
+                3 ->
+                    "rd"
+
+                _ ->
+                    "th"
 
 
 {-| Convert an integer into an English ordinal number string (like `"4th"`).
